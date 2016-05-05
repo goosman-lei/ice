6ff2ecdb864b12a284c90372cc21869db550a0dd
leiguoguo <leiguoguo@oneniceapp.com>
2016-05-05 14:22:34 +0800

    修改别名实现

---
+++ class_alias.php
@@ -0,0 +1,16 @@
+<?php
+/* 类名映射 */
+class_alias('\\Ice\\Frame\\Config',             'F_Config');
+class_alias('\\Ice\\Frame\\Ice',                'F_Ice');
+class_alias('\\Ice\\Frame\\App',                'F_App');
+class_alias('\\Ice\\Frame\\Logger',             'F_Logger');
+class_alias('\\Ice\\Frame\\Error\\Code',        'F_ECode');
+
+class_alias('\\Ice\\Frame\\Web\\Action',        'FW_Action');
+
+class_alias('\\Ice\\Util\\Time',                'U_Time');
+class_alias('\\Ice\\Util\\Env',                 'U_Env');
+class_alias('\\Ice\\Util\\DString',             'U_String');
+class_alias('\\Ice\\Util\\DMap',                'U_Map');
+class_alias('\\Ice\\Util\\DStub',               'U_Stub');
+class_alias('\\Ice\\Util\\DArray',              'U_Array');
