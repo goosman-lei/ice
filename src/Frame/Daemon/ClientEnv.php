f5371e9aa370179abcf53236136e853da4c3e746
leiguoguo <leiguoguo@oneniceapp.com>
2016-05-10 18:54:25 +0800

    fix

--- src/Frame/Daemon/ClientEnv.php
+++ src/Frame/Daemon/ClientEnv.php
@@ -1,6 +1,6 @@
 <?php
 namespace Ice\Frame\Daemon;
-class ClientEnv \Ice\Frame\Abs\ClientEnv {
+class ClientEnv extends \Ice\Frame\Abs\ClientEnv {
 
     public function __construct() {
         parent::__construct();
