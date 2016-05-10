9cf44ca9dd99c65b4214d0ab61920327ed43170a
leiguoguo <leiguoguo@oneniceapp.com>
2016-05-10 15:14:34 +0800

    modify demo

--- template/web/tpl_action_Say_Helloworld.php
+++ template/web/tpl_action_Say_Helloworld.php
@@ -2,11 +2,13 @@
 namespace ${PROJECT_NAMESPACE}\Action\Say;
 class Helloworld extends \FW_Action {
     public function execute() {
+        $client = new \Ice\Frame\Service\Client('http://ice-service.leiguoguo.lab.niceprivate.com', 'Say');
         return array(
             'code' => 0,
             'data' => array(
                 'uid'   => 5012470,
                 'uname' => 'goosman-lei',
+                'service' => $client->hello('Jack'),
             ),
         );
     }
