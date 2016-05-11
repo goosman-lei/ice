abd988a89bf0d434718cb27c8cb012a730f57be2
leiguoguo <leiguoguo@oneniceapp.com>
2016-05-11 16:20:35 +0800

    update

--- tpl.web/src/service/Say.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace ${PROJECT_NAMESPACE}\Service;
-class Say extends \FS_Service {
-    public function hello($name) {
-        return $this->succ('Hello ' . $name);
-    }
-}
--- tpl.web/src/smarty-tpl/say/helloworld.tpl
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>Demo</title>
-</head>
-<body>
-    UNAME: {%$data.uname%}
-</body>
-</html>
