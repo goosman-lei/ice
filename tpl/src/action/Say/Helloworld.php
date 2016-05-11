919a38f17543c31fd7b8c45a649993f3c8652ee9
leiguoguo <leiguoguo@oneniceapp.com>
2016-05-11 14:55:17 +0800

    fix

--- tpl/src/action/Say/Helloworld.php
+++ tpl/src/action/Say/Helloworld.php
@@ -2,7 +2,7 @@
 namespace ${PROJECT_NAMESPACE}\Action\Say;
 class Helloworld extends \FW_Action {
     public function execute() {
-        $client = $this->ice->mainApp->proxy_resource->get('demo-remote', 'Say');
+        $client = $this->ice->mainApp->proxy_service->get('demo-remote', 'Say');
         return array(
             'code' => 0,
             'data' => array(
