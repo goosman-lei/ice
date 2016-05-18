7495a2942ea4ce5b02c0f0dbc35636c134657909
goosman.lei <goosman.lei@gmail.com>
2016-05-18 23:06:32 +0800

    FixBug: 路由时使用默认controller/action配置错误

--- src/Frame/Web/Router/RQuery.php
+++ src/Frame/Web/Router/RQuery.php
@@ -5,8 +5,8 @@ class RQuery {
         $class  = $request->getQuery('c');
         $action = $request->getQuery('a');
 
-        $request->class  = ucfirst(strtolower(isset($class) ? $class : \F_Ice::$ins->mainAppConf['runner']['frame']['default_class']));
-        $request->action = ucfirst(strtolower(isset($action) ? $action : \F_Ice::$ins->mainAppConf['runner']['frame']['default_action']));
+        $request->class  = ucfirst(strtolower(isset($class) ? $class : \F_Ice::$ins->runner->mainAppConf['runner']['frame']['default_class']));
+        $request->action = ucfirst(strtolower(isset($action) ? $action : \F_Ice::$ins->runner->mainAppConf['runner']['frame']['default_action']));
 
         $response->class  = $request->class;
         $response->action = $request->action;
--- src/Frame/Web/Router/RStatic.php
+++ src/Frame/Web/Router/RStatic.php
@@ -4,8 +4,8 @@ class RStatic {
     public function route($request, $response) {
         preg_match(';^/(\w+)?(?:/(\w+)?)?;', $request->uri, $match);
 
-        $request->class  = ucfirst(strtolower(isset($match[1]) ? $match[1] : \F_Ice::$ins->mainAppConf['runner']['frame']['default_class']));
-        $request->action = ucfirst(strtolower(isset($match[2]) ? $match[2] : \F_Ice::$ins->mainAppConf['runner']['frame']['default_action']));
+        $request->class  = ucfirst(strtolower(isset($match[1]) ? $match[1] : \F_Ice::$ins->runner->mainAppConf['runner']['frame']['default_class']));
+        $request->action = ucfirst(strtolower(isset($match[2]) ? $match[2] : \F_Ice::$ins->runner->mainAppConf['runner']['frame']['default_action']));
 
         $response->class  = $request->class;
         $response->action = $request->action;
