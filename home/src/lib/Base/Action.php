4055f6b50f39380299bead367462555bd4621ada
goosman.lei <goosman.lei@gmail.com>
2016-05-19 11:36:29 +0800

    修正为简版主站

--- home/src/lib/Base/Action.php
+++ home/src/lib/Base/Action.php
@@ -8,19 +8,5 @@ class Action extends \FW_Action {
     }
 
     protected function init() {
-        $navigationService = $this->ice->workApp->proxy_service->get('internal', 'Navigation');
-        $retArr = $navigationService->getAllNodes($this->request->class, $this->request->action);
-
-        if ($retArr['code'] == 0) {
-            $this->response->addTplData(array(
-                'rootNavs'    => $retArr['data']['rootNavs'],
-                'currNavRoot' => $retArr['data']['currNavRoot'],
-            ));
-        } else {
-            $this->response->addTplData(array(
-                'rootNavs'    => array(),
-                'currNavRoot' => array(),
-            ));
-        }
     }
 }
--- home/src/smarty-tpl/homepage/index.tpl
+++ home/src/smarty-tpl/homepage/index.tpl
@@ -1,3 +1,6 @@
 {extends file='_common/layout-homepage.tpl'}
 {block name=body}
+<h3>Ice Framework开发历程</h3>
+<p></p>
+<h3>Ice Framework设计思想</h3>
 {/block}
