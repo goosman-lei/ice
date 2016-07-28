5a4fd25056d1ed26788ceea05a30b875bb1e56c0
chenlipeng <chenlipeng@oneniceapp.com>
2016-07-28 11:24:06 +0800

    fixbug

--- src/Frame/Embeded/Response.php
+++ src/Frame/Embeded/Response.php
@@ -2,10 +2,5 @@
 namespace Ice\Frame\Embeded;
 class Response extends \Ice\Frame\Abs\Response {
     public function error($errno, $data = array()) {
-        $this->code = $code;
-        $this->data = $data;
-
-        echo $this->serialize();
-        exit(1);
     }
 }
