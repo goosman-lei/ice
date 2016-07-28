90d4768019c0681d5d809c9d72e9d898f096dc52
chenlipeng <chenlipeng@oneniceapp.com>
2016-07-28 11:15:04 +0800

    fixbug: PHP Fatal error:  Call to undefined method Ice\Frame\Embeded\Response::error()

--- src/Frame/Embeded/Response.php
+++ src/Frame/Embeded/Response.php
@@ -1,4 +1,11 @@
 <?php
 namespace Ice\Frame\Embeded;
 class Response extends \Ice\Frame\Abs\Response {
+    public function error($errno, $data = array()) {
+        $this->code = $code;
+        $this->data = $data;
+
+        echo $this->serialize();
+        exit(1);
+    }
 }
