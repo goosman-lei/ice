d4a171072ebdfb4b1b11e8c26ddbed64b8d7839e
windrainw <windrainw@gmail.com>
2016-11-28 21:14:59 +0800

    web方式支持多app

--- src/Frame/Web/UnitTest.php
+++ src/Frame/Web/UnitTest.php
@@ -12,11 +12,7 @@ abstract class UnitTest extends \PHPUnit_Framework_TestCase {
             $runner = new \Ice\Frame\Runner\Web($projectRootPath . '/src/conf/app.php');
             $runner->run('ut');
         }
-
-        if (\F_Ice::$ins->runner->name != 'web') {
-            throw new \Exception('In one UnitTest process, don\'t allow multi runner');
-        }
-    }
+   }
 
     protected function callAction($class, $action) {
         return \F_Ice::$ins->runner->callAction($class, $action);
